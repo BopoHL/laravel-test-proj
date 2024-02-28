1e3685b65d0f974b6d63d2b4cb67503023000ec8
Leonid Voronin <127386605+BopoHL@users.noreply.github.com>
2024-02-28 17:36:12 +0500

    Add command to send mails

---
+++ app/Console/Commands/SendBirthdayEmailCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Contracts\IUserRepository;
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class SendBirthdayEmailCommand extends Command
+{
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-birthday-email-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send birthday greetings to user';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(IUserRepository $repository)
+    {
+        $user = $repository->getUserByEmail('lvoronin99@gmail.com');
+//        Mail::to($user->email)
+    }
+}
